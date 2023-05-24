b8dcab9835991926deea119237a60b87a526199f
Christian Oviedo <Chris_ovi1@hotmail.com>
2023-05-23 22:07:23 -0700

    read for cart done

--- react-app/src/components/Cart/index.js
+++ react-app/src/components/Cart/index.js
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from "react-redux"
+import './cart.css'
 
 
 
@@ -12,21 +13,43 @@ const ShoppingCart = () => {
         <>
             <h1>Shopping Cart</h1>
 
-            <div>
-                <div>
+            <div className="cart-block">
+                <div className="cart-games">
                     {userCartArr.map((game) => {
                         return (
-                            <div key={game.id}>
-                                <div>
-                                    
+                            <div key={game.id} className="cart-indiv-game-info">
+                                <div className="cart-indiv-game-left">
+                                    <img src={game.game_info.main_img} className="cart-game-img" />
+                                    <div className="cart-indiv-game-name">
+                                        {game.game_info.game_name}
+                                    </div>
+                                </div>
+
+                                <div className="cart-indiv-game-right">
+                                    <div>
+                                        ${game.game_info.price}
+                                    </div>
+                                    <button className="cart-remove-game">
+                                        Remove
+                                    </button>
                                 </div>
                             </div>
                         )
                     })}
                 </div>
 
-                <div>
-                    total
+                <div className="cart-total">
+                    <div className="cart-estimate">
+                        <div>
+                            Estimated total
+                        </div>
+                        <div>
+                            $total price
+                        </div>
+                    </div>
+                    <div className="cart-purchase">
+                        <button>Purchase for myself</button>
+                    </div>
                 </div>
             </div>
 
